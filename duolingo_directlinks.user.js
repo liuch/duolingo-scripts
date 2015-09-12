0f815a3e58bbf9995a3aa98fc0fffcd250f53606
liuch <liuch@mail.ru>
2015-09-13 01:49:05 +0300

    fixed a typo

--- duolingo_directlinks.user.js
+++ duolingo_directlinks.user.js
@@ -5,7 +5,7 @@
 // @version        0.3.3
 // @grant          none
 // @description    This script adds the direct links for discussion comments, translation sentences, and activity stream events
-// @description:ru Этот скрипт добавляет прямые ссылки на комментария в форумах, на предложения в переводах и на события в ленте
+// @description:ru Этот скрипт добавляет прямые ссылки на комментарии в форумах, на предложения в переводах и на события в ленте
 // @updateURL      https://github.com/liuch/duolingo-scripts/raw/master/duolingo_directlinks.meta.js
 // @downloadURL    https://github.com/liuch/duolingo-scripts/raw/master/duolingo_directlinks.user.js
 // @author         FieryCat aka liuch
