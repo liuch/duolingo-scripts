72cd9c5347b9ec973129002e2b82877d8484c2b6
liuch <liuch@mail.ru>
2015-09-13 22:48:50 +0300

    DuoLessonsFix: added the description text

--- duolingo_lessonsfix.user.js
+++ duolingo_lessonsfix.user.js
@@ -4,8 +4,8 @@
 // @include        https://www.duolingo.com/*
 // @version        0.1.3
 // @grant          none
-// @description    TODO
-// @description:ru TODO
+// @description    This script pauses the timer between exercises in the timed practice.
+// @description:ru Этот скрипт ставит таймер на паузу между заданиями в тренировке на время.
 // @updateURL      https://github.com/liuch/duolingo-scripts/raw/master/duolingo_lessonsfix.meta.js
 // @downloadURL    https://github.com/liuch/duolingo-scripts/raw/master/duolingo_lessonsfix.user.js
 // @author         FieryCat aka liuch
