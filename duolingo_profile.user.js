663c0f1c6b664f2a9f24e7cf126812c94337345e
liuch <liuch@mail.ru>
2022-05-13 18:16:13 +0300

    DuoProfile: bump version

--- duolingo_profile.user.js
+++ duolingo_profile.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://github.com/liuch/duolingo-scripts
 // @include        https://www.duolingo.com/*
 // @include        https://preview.duolingo.com/*
-// @version        1.16.0
+// @version        1.17.0
 // @grant          none
 // @description    This script displays additional information in the users' profile.
 // @description:ru Этот скрипт показывает дополнительную информацию в профилях пользователей.
